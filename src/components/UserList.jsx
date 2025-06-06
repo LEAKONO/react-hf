af1d383e605a72bbe5d3aa37a06f0ec8ba9b8d49
LEAKONO <leakonoemmanuel3@gmail.com>
2025-06-06 15:12:32 +0300

    feat: setup basic folder structure for components and pages

---
+++ src/components/UserList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+function UserList() {
+    const[users,setUsers]=useState([])
+    useEffect(()=>{
+        
+    })
+  return (
+    <div>UserList</div>
+  )
+}
+
+export default UserList
